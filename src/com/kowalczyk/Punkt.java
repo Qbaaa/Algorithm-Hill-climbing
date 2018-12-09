7807d948cb2c6efd1d278be6ed78f5e4960f71e6
Qbaaa <jakubkowalczyk95@gmail.com>
2018-12-09 16:07:56 +0100

    adding to the class Punkt with implemetation

---
+++ src/com/kowalczyk/Punkt.java
@@ -0,0 +1,19 @@
+package com.kowalczyk;
+
+class Punkt
+{
+    int x;
+    int y;
+
+    public Punkt()
+    {
+        this.x=0;
+        this.y=0;
+    }
+
+    public Punkt(int _x, int _y)
+    {
+        this.x = _x;
+        this.y = _y;
+    }
+}
